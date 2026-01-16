3e875c9e8073bd8fcf621dcf3f69d572bdff3dd2
ktw <pisces3356@gmail.com>
2026-01-16 23:41:52 +0800

    chore: 移除 FISH 目錄（已遷移至 ktw-saas-bot/channel_manager）

--- FISH/src/platforms/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Platforms 模組"""
-from .base import BasePlatform
-from .agoda import AgodaPlatform
-from .booking import BookingPlatform
-
-__all__ = ["BasePlatform", "AgodaPlatform", "BookingPlatform"]
--- FISH/src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Utils 模組"""
-from .logger import logger
-
-__all__ = ["logger"]
